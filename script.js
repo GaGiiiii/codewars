513735d0bc8e269586c2c74b5777afcfe20b36ab
GaGiiiii <dragoslav.gagi8@gmail.com>
2022-03-29 19:14:17 +0200

    First Commit

---
+++ script.js
@@ -0,0 +1,594 @@
+// let followers = [
+
+//   "mladi_ivkovic",
+//   "kozmetika_iz__prirode__",
+//   "zdravko.mrgud",
+//   "jogurat",
+//   "il_stefan",
+//   "fonis_fon",
+//   "markovic.aleksandraa",
+//   "gonzales_dostava_28",
+//   "masavucic5",
+//   "dekanti_sillage",
+//   "kaca__stanic",
+//   "charlie_4.7",
+//   "sandra_1dj",
+//   "sanjaaaaaaa.a",
+//   "itkonekt",
+//   "grubic_dusko",
+//   "markooo97",
+//   "georg_von_belgrad",
+//   "telefonbackupp",
+//   "csisthebest10",
+//   "majaratkovic",
+//   "lepsimetej",
+//   "anitacabarkapa",
+//   "ninakaurin",
+//   "djikanovicn",
+//   "ksenijamilen",
+//   "bane_x8",
+//   "brankajankovitsh",
+//   "bozovicstefan",
+//   "anastasijaivanovic_",
+//   "stefan_milanovich",
+//   "ivetic4",
+//   "migrutin",
+//   "nastasjaa_b",
+//   "krilati_fenix",
+//   "sava_stanimirovic",
+//   "nemanja_ph0to",
+//   "cikic.m.radovan",
+//   "miljan.simonovic",
+//   "teodora_copic",
+//   "arodissim",
+//   "uki_stan",
+//   "v_lazarevic6",
+//   "poozitiva",
+//   "sankkov_",
+//   "pedja.tanaskovic",
+//   "f.radosavljevic",
+//   "just_rile",
+//   "ana.stojcic",
+//   "brankovic_nebojsa",
+//   "aleksa10",
+//   "kedziiiii",
+//   "srbenda.fonis",
+//   "kostadept",
+//   "miljkovic_luka",
+//   "petarllic",
+//   "dekant_za",
+//   "tosha.exe",
+//   "sonyx.iznajmljivanje",
+//   "bolestas",
+//   "tatamata.casovi",
+//   "princpop33",
+//   "djordjesaric6.jpg",
+//   "jovan.jovanovic8",
+//   "stex13_",
+//   "unabosnic",
+//   "kristinaa247",
+//   "gym_paja",
+//   "nikola.mirilo",
+//   "dekanti_orginal_parfema",
+//   "bajkulica",
+//   "zekaaa15",
+//   "milena.djordja",
+//   "_dorotejam_",
+//   "99marko",
+//   "max_marijana",
+//   "happy.butik.kg",
+//   "parfemi_oriiginal_",
+//   "maria.papaha",
+//   "savicevicmm",
+//   "slusalice_i_iphone_maskice",
+//   "_that_daniel",
+//   "noosrep",
+//   "blizanciica",
+//   "caffe_pub22",
+//   "nevenaa_k",
+//   "stefan_krstovic",
+//   "dekant_stefan",
+//   "prodaja_garderobe_fashio",
+//   "codeandmeme",
+//   "onepro.rs",
+//   "dunja_nesic",
+//   "djeric_milena",
+//   "rajkovic_milos",
+//   "cizmic.t",
+//   "mirkozecic",
+//   "qualitymediastation",
+//   "mila_cvetkovicc",
+//   "teodora_gavi",
+//   "milos.98",
+//   "dzeca_m",
+//   "aleksicm__263",
+//   "vajagicb",
+//   "anjazivkovic",
+//   "kovicaleksandra",
+//   "danilogacevic.js",
+//   "st_f_nj_n_c",
+//   "iva_balotic",
+//   "aleksa.was.here",
+//   "v_jovanovic98",
+//   "djole0812",
+//   "jovana_obradovic",
+//   "nikolicjovana_",
+//   "marijaaorlic",
+//   "p_e_g_y",
+//   "kefalinho",
+//   "___idontcareee___",
+//   "hatsep_sut",
+//   "_cvetaa_",
+//   "jankovic_a11",
+//   "dzodan._",
+//   "tapuskovicm",
+//   "gethoven_",
+//   "milanculic97",
+//   "frizerski_salon_petar",
+//   "djkristinaaa",
+//   "ili_ti_ilija",
+//   "_eljko_srdi_",
+//   "sicovicc",
+//   "mdjordjevic97",
+//   "vaske_7",
+//   "zahar_m_",
+//   "privantan",
+//   "milos__mijailovic",
+//   "nikolacolic33",
+//   "vivi_ca98",
+//   "gimili_ue",
+//   "helena.janjusevic",
+//   "_zurnic_",
+//   "pixelate_visuals",
+//   "milosninkovic5",
+//   "jovicaa03",
+//   "_marija_jovic_62",
+//   "_swans",
+//   "monsieurlaki",
+//   "djordjekaragaca",
+//   "way.too.aesthetic",
+//   "djuranijus",
+//   "zivkovic__i",
+//   "_suncica_s",
+//   "_stod__",
+//   "aegnorr",
+//   "predragimsic",
+//   "igic_28",
+//   "popz.zone",
+//   "filipdespic",
+//   "jachovich",
+//   "durac457",
+//   "matejaivanovic23",
+//   "miliboo_",
+//   "__djodjo_",
+//   "danilo_djuro",
+//   "__no.yes__",
+//   "stefaneic",
+//   "nikola_jevtovic",
+//   "p_jay_free",
+//   "zhorzhersi",
+// ]
+
+// let following = [
+// "mladi_ivkovic",
+// "tvojckode",
+// "apstraktnivicevi",
+// "zdravko.mrgud",
+// "markostanic_sw",
+// "jogurat",
+// "il_stefan",
+// "markovic.aleksandraa",
+// "pkzb_puki",
+// "masavucic5",
+// "kaca__stanic",
+// "charlie_4.7",
+// "sandra_1dj",
+// "sanjaaaaaaa.a",
+// "grubic_dusko",
+// "markooo97",
+// "georg_von_belgrad",
+// "lepsimetej",
+// "majaratkovic",
+// "anitacabarkapa",
+// "ninakaurin",
+// "djikanovicn",
+// "ksenijamilen",
+// "bane_x8",
+// "brankajankovitsh",
+// "bozovicstefan",
+// "anastasijaivanovic_",
+// "stefan_milanovich",
+// "ivetic4",
+// "migrutin",
+// "nastasjaa_b",
+// "krilati_fenix",
+// "sava_stanimirovic",
+// "nemanja_ph0to",
+// "cikic.m.radovan",
+// "miljan.simonovic",
+// "teodora_copic",
+// "arodissim",
+// "uki_stan",
+// "v_lazarevic6",
+// "poozitiva",
+// "sankkov_",
+// "pedja.tanaskovic",
+// "f.radosavljevic",
+// "ana.stojcic",
+// "just_rile",
+// "brankovic_nebojsa",
+// "aleksa10",
+// "srbenda.fonis",
+// "kedziiiii",
+// "kostadept",
+// "miljkovic_luka",
+// "petarllic",
+// "tosha.exe",
+// "bolestas",
+// "popz.studios",
+// "tatamata.casovi",
+// "princpop33",
+// "djordjesaric6.jpg",
+// "stex13_",
+// "unabosnic",
+// "kristinaa247",
+// "webapp_creator",
+// "gym_paja",
+// "nikola.mirilo",
+// "dekanti_orginal_parfema",
+// "bajkulica",
+// "zekaaa15",
+// "milena.djordja",
+// "99marko",
+// "_dorotejam_",
+// "max_marijana",
+// "savicevicmm",
+// "_that_daniel",
+// "blizanciica",
+// "stefan_krstovic",
+// "nevenaa_k",
+// "dekant_stefan",
+// "prodaja_garderobe_fashio",
+// "fonis_fon",
+// "dunja_nesic",
+// "onepro.rs",
+// "djeric_milena",
+// "cizmic.t",
+// "rajkovic_milos",
+// "mirkozecic",
+// "zheson_",
+// "mila_cvetkovicc",
+// "dzeca_m",
+// "milos.98",
+// "teodora_gavi",
+// "aleksicm__263",
+// "vajagicb",
+// "anjazivkovic",
+// "kovicaleksandra",
+// "st_f_nj_n_c",
+// "danilogacevic.js",
+// "iva_balotic",
+// "aleksa.was.here",
+// "v_jovanovic98",
+// "djole0812",
+// "jovana_obradovic",
+// "nikolicjovana_",
+// "marijaaorlic",
+// "p_e_g_y",
+// "kefalinho",
+// "___idontcareee___",
+// "hatsep_sut",
+// "_cvetaa_",
+// "jankovic_a11",
+// "dzodan._",
+// "tapuskovicm",
+// "gethoven_",
+// "milanculic97",
+// "frizerski_salon_petar",
+// "djkristinaaa",
+// "filipdespic",
+// "ili_ti_ilija",
+// "_eljko_srdi_",
+// "sicovicc",
+// "mdjordjevic97",
+// "vaske_7",
+// "zahar_m_",
+// "privantan",
+// "milos__mijailovic",
+// "nikolacolic33",
+// "vivi_ca98",
+// "gimili_ue",
+// "helena.janjusevic",
+// "_zurnic_",
+// "milosninkovic5",
+// "_swans",
+// "djordjekaragaca",
+// "monsieurlaki",
+// "way.too.aesthetic",
+// "djuranijus",
+// "zivkovic__i",
+// "_suncica_s",
+// "_stod__",
+// "predragimsic",
+// "aegnorr",
+// "igic_28",
+// "popz.zone",
+// "jachovich",
+// "matejaivanovic23",
+// "durac457",
+// "miliboo_",
+// "__djodjo_",
+// "danilo_djuro",
+// "__no.yes__",
+// "stefaneic",
+// "nikola_jevtovic",
+// "pixelate_visuals",
+// "zhorzhersi",
+// "p_jay_free",
+// ]
+
+// let peopleThatDontFollowMe = [];
+// let peopleThatIDontFollow = [];
+
+// for(let i = 0; i < following.length; i++){
+//   if(!existsInArray(following[i], followers)){
+//     peopleThatDontFollowMe.push(following[i]);
+//   }
+// }
+
+// for(let i = 0; i < followers.length; i++){
+//   if(!existsInArray(followers[i], following)){
+//     peopleThatIDontFollow.push(followers[i]);
+//   }
+// }
+
+// function existsInArray(item, array){
+//   for(let i = 0; i < array.length; i++){
+//     if(item == array[i]){
+//       return true;
+//     }
+//   }
+
+//   return false;
+// }
+
+// console.log(peopleThatDontFollowMe);
+// console.log(peopleThatIDontFollow);
+
+
+
+
+
+
+// function fatCat(arr, K) {
+//   let sums = [];
+//   let hashTable = {};
+
+//   for (let i = 0; i < arr.length; i++) {
+//     let sumMinusElement = K - arr[i];
+
+//     if (hashTable[sumMinusElement.toString()] !== undefined) {
+//       sums.push([hashTable[sumMinusElement.toString()].index, i]);
+//     }
+
+//     hashTable[arr[i].toString()] = { value: arr[i], index: i };
+//   }
+
+//   return sums;
+// }
+
+// console.log(fatCat([1, 3, 1, 2, 8, 4], 7));
+
+
+
+
+
+// function calcAvgHeight(data) {
+//   // Calculate average height from received data. If no data, return null.
+//   if(data === null || data === undefined || Object.keys(data).length === 0){
+//     return null;
+//   }
+
+//   let total = 0;
+
+//   for(const key in data){
+//     total += data[key].height;
+//   }
+
+//   return total / Object.keys(data).length;
+// }
+
+// var avgHeight = calcAvgHeight({
+//   Matt: { height: 174, weight: 69 },
+//   Jason: { height: 190, weight: 103 }
+// });
+// console.log(avgHeight);
+
+// function setup() {
+//   // Write your code here.
+//   $("#postComment").click(() => {
+//     let commentValue = $("#comment").val();
+
+//     if (commentValue === '') {
+//       return;
+//     }
+
+//     $("#commentList").append(`<li>${commentValue}</li>`);
+//     $("#comment").val("");
+//   });
+// }
+
+// // Example case. 
+// document.body.innerHTML = `
+// <ul id='commentList'>
+// </ul>
+// <form>
+//   <input type='text' id='comment'/>
+//   <input type='button' id='postComment' value='Post'/>
+// </form>`;
+
+// setup();
+
+// $("#comment").val("test");
+// // $("#postComment").click();
+// console.log(document.body.innerHTML);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+'use strict';
+
+const fs = require('fs');
+
+process.stdin.resume();
+process.stdin.setEncoding('utf-8');
+
+let inputString = '';
+let currentLine = 0;
+
+process.stdin.on('data', function (inputStdin) {
+    inputString += inputStdin;
+});
+
+process.stdin.on('end', function () {
+    inputString = inputString.split('\n');
+
+    main();
+});
+
+function readLine() {
+    return inputString[currentLine++];
+}
+
+
+/*
+ * Complete the 'prison' function below.
+ *
+ * The function is expected to return a LONG_INTEGER.
+ * The function accepts following parameters:
+ *  1. INTEGER n
+ *  2. INTEGER m
+ *  3. INTEGER_ARRAY h
+ *  4. INTEGER_ARRAY v
+ */
+
+function createMatrix(n, m) {
+    let arr = new Array(n);
+
+    for (let i = 0; i < arr.length; i++) {
+        arr[i] = new Array(m);
+    }
+
+    for (let i = 0; i < n; i++) {
+        for (let j = 0; j < m; j++) {
+            arr[i][j] = { b: 1, r: 1 };
+        }
+    }
+
+    return arr;
+}
+
+function removeRows(arr, rows, n, m) {
+    for (let i = 0; i < rows.length; i++) {
+        for (let j = rows[i] - 1; j < rows[i]; j++) {
+            for (let k = 0; k < m; k++) {
+                let copy = {...arr[j][k]};
+                copy.b = 0;
+                arr[j][k] = copy;
+            }
+        }
+    }
+    
+    return arr;
+}
+
+function removeCols(arr, cols, n, m) {
+    for (let i = 0; i < cols.length; i++) {
+        for (let j = 0; j < n; j++) {
+            for (let k = cols[i] - 1; k < cols[i]; k++) {
+                let copy = {...arr[j][k]};
+                copy.r = 0;
+                arr[j][k] = copy;
+            }
+        }
+    }
+    
+    return arr;
+}
+
+function findBiggestRowHole(arr,n,m){
+    let maxCounter = -1;
+    
+    for(let i = 0; i < n - 1; i++){
+        let counter = 1;
+        
+        for(let j = i+1; j < n; j++){
+            if(arr[j][0].b === 0){
+                counter++;
+            }
+        }
+        if(arr[i][0].b === 0){
+            counter++;
+        }
+        if(counter > maxCounter){
+            maxCounter = counter;
+        }
+    }
+    
+    return maxCounter;
+}
+
+function findBiggestColHole(arr,n,m){
+    let maxCounter = -1;
+    
+    for(let i = 0; i < m - 1; i++){
+        let counter = 1;
+        
+        for(let j = i+1; j < m; j++){
+            if(arr[0][j].r === 0){
+                counter++;
+            }
+        }
+        if(arr[0][i].r === 0){
+            counter++;
+        }
+        if(counter > maxCounter){
+            maxCounter = counter;
+        }
+    }
+    
+    return maxCounter;
+}
+
+function prison(n, m, h, v) {
+    // Write your code here
+    let arr = createMatrix(n, m);
+    arr = removeRows(arr, h, n, m);
+    arr = removeCols(arr, v, n, m);
+    console.log(findBiggestRowHole(arr, n, m));
+
+    return findBiggestRowHole(arr, n,m) * findBiggestColHole(arr, n,m);
+}
+
+
+
+
+
+
+
+
+
+
+
